320a151f47648ffcef8608f03aa03048e2c6ca17
Nitishbose <bose.nitish2@gmail.com>
2023-02-12 13:10:50 +0530

    Add project files.

---
+++ BookListRazor/Program.cs
@@ -0,0 +1,41 @@
+using BookListRazor.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddDbContext<ApplicationDbContext>(
+                option=> option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapRazorPages();
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
